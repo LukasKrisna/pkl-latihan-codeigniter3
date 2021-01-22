f9822304edefba73de96f3af5c5decaf4f1117b4
LukasKrisna <krisnaprasriyan@gmail.com>
2021-01-22 23:26:59 +0700

    fitur login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -11,10 +11,18 @@ class Auth extends CI_Controller
 
     public function index ()
     {
-        $data['title'] = 'Login';
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/login');
-        $this->load->view('templates/auth_footer');
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+        if ( $this->form_validation->run() == false ) {
+            $data['title'] = 'Login';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('templates/auth_footer');
+        } else {
+          $this->_login();  
+        }
+            
     }
 
     public function registration()
@@ -51,5 +59,44 @@ class Auth extends CI_Controller
             Akun berhasil ter-registrasi!</div>');
             redirect('auth');
         }
+    }
+
+    private function _login()
+    {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+        
+        if ( $user ) {
+
+            if ( $user['is_active'] == 1 ) {
+
+                if ( password_verify($password, $user['password']) ) {
+                    $data = [
+                        'email'     => $user['email'],
+                        'role_id'   => $user['role_id']
+                    ];
+                    $this->session->set_userdata($data);
+                    redirect('user');
+
+                }else {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                    Password salah</div>');
+                    redirect('auth');  
+                }
+
+            }else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                Email belum di-aktivasi</div>');
+                redirect('auth');    
+            }
+
+        }else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+            Email belum ter-registrasi</div>');
+            redirect('auth');
+        }
+        
     }
 }
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -17,16 +17,18 @@
                                         <h1 class="h4 text-gray-900 mb-4">Login</h1>
                                     </div>
 
-                                    <?php echo $this->session->flashdata('register') ?>
+                                    <?php echo $this->session->flashdata('message') ?>
 
-                                    <form class="user">
+                                    <form class="user" method="post" action="<?php echo base_url('auth')?>">
                                         <div class="form-group">
                                             <input type="text" class="form-control form-control-user"
-                                                id="email" name="email" placeholder="Enter Email Address...">
+                                                id="email" name="email" placeholder="Enter Email Address..." value="<?php echo set_value('email');?>">
+                                                <?php echo form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                                         </div>
-                                        <div class="form-group">
+                                        <div class="form-group" method="post" action="<?php echo base_url('auth')?>">
                                             <input type="password" class="form-control form-control-user"
                                                 id="password" name="password" placeholder="Password">
+                                                <?php echo form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
                                         </div>
                                         <button type="submit" class="btn btn-primary btn-user btn-block">
                                             Login
